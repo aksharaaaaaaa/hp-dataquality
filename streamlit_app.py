7beb96dd3517561cf8e52ab29f9d21d7adee79e4
aksharaaaaaaa <167548924+aksharaaaaaaa@users.noreply.github.com>
2024-09-27 13:06:39 +0000

    created streamlit dashboard

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,156 @@
 import streamlit as st
 import pandas as pd
-import math
 from pathlib import Path
+import altair as alt
+from streamlit_extras.dataframe_explorer import dataframe_explorer
+from streamlit_extras.stylable_container import stylable_container
+import plotly.express as px
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
+hp_data = Path(__file__).parent/'data/train.csv'
+hp_df = pd.read_csv(hp_data)
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
+st.title("Data Quality Assessment - House Prices :house_buildings:")
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+st.divider()
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+st.header(':blue[Data completeness]', help='Count null values in each column')
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+labels = hp_df.columns.tolist()
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+total_null_values = hp_df.isnull().sum()
+total_values = hp_df.isnull().sum() + hp_df.notnull().sum()
+#hp_df.count().sort_values(ascending=True)
+not_null_values = hp_df.notnull().sum()
+null_values_percentage = (total_null_values/total_values)*100
 
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
+missing_values = pd.concat({'Null': total_null_values, 'Not_null': not_null_values}, axis=1)#, 'Percentage_missing': null_values_percentage}, axis=1)
+missing_values = missing_values.transpose()
+missing_values.reset_index()
+miss = pd.concat({'Null': total_null_values, 'Not_null': not_null_values, 'Percentage_missing': null_values_percentage}, axis=1)
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
+#st.dataframe(dataframe_explorer(miss,case=False))
 
-    return gdp_df
+miss_noindex = miss.reset_index()
 
-gdp_df = get_gdp_data()
+chart = alt.Chart(miss_noindex).mark_bar().encode(
+    x=alt.X(miss_noindex.columns[3], type='quantitative', title='Percentage missing (%)'),
+    y=alt.Y(miss_noindex.columns[0], type='nominal', title='Column')).properties(title='Missing data (all columns)')
+text = alt.Chart(miss_noindex).mark_text(dx=15,dy=3,color='white').encode(
+    x=alt.X(miss_noindex.columns[3], type='quantitative'),
+    y=alt.Y(miss_noindex.columns[0], type='nominal'),
+    text=alt.Text('Percentage_missing', format='.1f'))
+st.altair_chart(chart+text, use_container_width=True)
 
-# -----------------------------------------------------------------------------
-# Draw the actual page
+st.info('The following columns intentionally contain NA values as non-applicable attributes:  \n\n Alley, BsmtQual, BsmtCond, BsmtExposure, BsmtFinType1, BsmtFinType2, FireplaceQu, GarageType, GarageFinish, GarageQual, GarageCond, PoolQC, Fence, MiscFeature',
+        icon="⚠️")
 
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
+NA_allowed_columns = ['Alley','BsmtQual','BsmtCond','BsmtExposure','BsmtFinType1',
+                      'BsmtFinType2','FireplaceQu','GarageType','GarageFinish',
+                      'GarageQual','GarageCond','PoolQC','Fence','MiscFeature']
 
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
+actual_NA_cols = []
+for y in hp_df.columns:
+    if y not in NA_allowed_columns:
+        actual_NA_cols.append(y)
 
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
+cols1= []
+nulls1 = []
+null_percent = []
+for z in hp_df.columns:
+    cols1.append(z)
+    if z in actual_NA_cols:
+        x = hp_df[z].isnull().sum()
+    else:
+        x = int('0')
+    nulls1.append(x)
+    null_df1 = pd.DataFrame({'C':cols1,'N':nulls1})
+    
+null_df1['P'] = (null_df1['N']/len(hp_df))*100
+null_df = null_df1.drop(null_df1[null_df1['N'] == 0].index)
 
-st.header(f'GDP in {to_year}', divider='gray')
+chart = alt.Chart(null_df).mark_bar().encode(
+    x=alt.X(null_df.columns[2], type='quantitative', title='Percentage missing (%)'),
+    y=alt.Y(null_df.columns[0], type='nominal', title='Column')).properties(title='Missing data (actual nulls)')
+text = alt.Chart(null_df).mark_text(dx=15,dy=3,color='white').encode(
+    x=alt.X(null_df.columns[2], type='quantitative'),
+    y=alt.Y(null_df.columns[0], type='nominal'),
+    text=alt.Text('P', format='.1f'))
+st.altair_chart(chart+text, use_container_width=True)
 
-''
 
-cols = st.columns(4)
+has_null = hp_df[actual_NA_cols].isna().any(axis=1)
+rows_with_null = has_null.sum()
 
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
+col1,col2 = st.columns(2)
+with col1:
+    lcont = stylable_container(key="cont1",
+                               css_styles="""
+                               {
+                               background-color: rgba(65, 135, 220, 0.1);
+                               color: white;
+                               border: 2px solid rgba(100, 140, 200, 0.8);
+                               border-radius: 10px;
+                               padding: calc(1em - 1px)}""")
+    lcont.write(':blue[Total rows processed]')
+    lcont.text(len(hp_df))
+with col2:
+    rcont = stylable_container(key="cont2",
+                               css_styles="""
+                               {
+                               background-color: rgba(220, 65, 70, 0.1);
+                               color: white;
+                               border: 2px solid rgba(210, 95, 95, 0.8);
+                               border-radius: 10px;
+                               padding: calc(1em - 1px)}""")
+    rcont.write(':red[Incomplete rows]')
+    rcont.text(rows_with_null)
 
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
 
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
+################################################################################
+### OUTLIERS
 
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+st.divider()
+st.header(':red[Outliers & statistics]',help = 'Identify outliers in each column')
+
+num_cols = ['Id','MSSubClass','LotFrontage','LotArea','OverallQual',
+            'OverallCond','YearBuilt','YearRemodAdd','MasVnrArea',
+            'BsmtFinSF1','BsmtFinSF2','BsmtUnfSF','TotalBsmtSF',
+            '1stFlrSF','2ndFlrSF','LowQualFinSF','GrLivArea',
+            'BsmtFullBath','BsmtHalfBath','FullBath','HalfBath',
+            'BedroomAbvGr','KitchenAbvGr','TotRmsAbvGrd','Fireplaces',
+            'GarageYrBlt','GarageCars','GarageArea','WoodDeckSF',
+            'OpenPorchSF','EnclosedPorch','3SsnPorch','ScreenPorch',
+            'PoolArea','MiscVal','MoSold','YrSold']
+
+tablist = st.tabs(num_cols)
+
+for i in range(len(tablist)):  # Use index for clarity
+    with tablist[i]:
+        fig = px.box(hp_df, x=num_cols[i], orientation='h',hover_data=['Id'], points='all', color_discrete_sequence=px.colors.qualitative.Pastel1)
+        st.plotly_chart(fig)
+
+col_stats = stylable_container(key="cont3",
+                               css_styles="""
+                               {
+                               background-color: rgba(220, 65, 70, 0.1);
+                               color: white;
+                               border: 2px solid rgba(210, 95, 95, 0.8);
+                               border-radius: 5px;
+                               padding: calc(1em - 1px)}""")
+with col_stats:
+    col_stats.subheader('Summary of column statistics')
+    col_stats.dataframe(hp_df.describe(), width = 670)#,use_container_width=True)
+
+#####################################################################
+st.divider()
+st.header(':green[Data uniqueness]', help='Identify duplicated rows within data')
+
+# unique value count in each column
+uniques = hp_df.nunique(axis='index').rename_axis("Columns").rename("Unique values")
+st.dataframe(uniques, use_container_width=True)
+
+# count duplicated rows
+dupes = hp_df.drop(columns=['Id'], axis=0).duplicated()
+st.write(f'The dataset contains :green[{dupes.sum()}] duplicated rows.')
+
